9dabdc18fa1f749e5f61c0ee6ddc901617327533
Erica Bryant <erica.bryant@me.com>
2016-12-04 20:16:39 -0800

    copying files

---
+++ ex17.py
@@ -0,0 +1,26 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+#we could do these two on one line, how?
+in_file = open(from_file)
+indata = in_file.read()
+
+#in_file = open(from_file, 'r')
+
+#print "The import file is %d bytes long" % len(indata)
+
+#print "Does the output file exist? %r" % exists(to_file)
+print "Ready? Hit RETURN to continue, CTRL-C to abort."
+raw_input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, All done."
+
+out_file.close()
+in_file.close()
